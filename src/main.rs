e2940e7c0de23dbb2bd8bc825d929d157a391e2d
Karim-W <karim.wael@gmail.com>
2023-03-04 22:54:14 +0400

    refactor. split and print in one line

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,11 @@ fn main() {
     }
     let pattern = &args[1];
     let stdin = io::stdin();
-    let mut matched_lines: Vec<String> = Vec::new();
     for line in stdin.lock().lines() {
         let line = line.expect("nothing to read");
-        if line.contains(pattern) {
-            matched_lines.push(line);
-        }
-    }
-    for line in matched_lines {
-        let split = line.split(pattern);
-        println!("{}", 
-         split.collect::<Vec<&str>>().
-         join(&format!("{}", pattern.red().bold().underline())));
+        let split: Vec<&str> = line.split(pattern).collect();
+        if split.len() > 1 {
+            println!("{}", split.join(&pattern.red().to_string()));
+        } 
     }
 }
